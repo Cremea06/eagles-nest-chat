ac9432831e4ebc4c712d71d87428d7c3252e51ad
Andy <andycremeans@yahoo.com>
2026-08-20 17:58:52 -0400

    Changed Title Back

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     <!DOCTYPE html>
     <html>
     <head>
-      <title>Eagle Nest Chat</title>
+      <title>Eagles Nest Chat</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 <style>
   * { box-sizing: border-box; margin: 0; padding: 0; }
